5d8104e319e05e78d23de17f8a4dfd9573f8a564
BrianDouglasIE <briandgls92@gmail.com>
2025-09-05 12:18:05 +0100

    clean relation structure

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import { Project } from "./models/project";
 import { db } from "./db/db";
 import type { projects_table } from "./sqlite-table-defs";
 
-const projectRaw = db.query<projects_table, []>('SELECT * FROM projects WHERE id = 1').get()
-if (projectRaw) {
-    const project = new Project(projectRaw.id, projectRaw.name, new Date(projectRaw.created_at))
-    console.log(project.getUsers());
+const rawProjects = db.query<projects_table, []>('SELECT * FROM projects LIMIT 10;').all()
+for (const rawProject of rawProjects) {
+    const project = new Project(rawProject.id, rawProject.name, new Date(rawProject.created_at))
+    console.log(project.name)
+    console.log(project.getUsers())
 }
--- models/project.ts
+++ models/project.ts
@@ -6,16 +6,13 @@ export type ProjectUser = { id: number, email: string, role: ProjectUserRole }
 export class Project {
     constructor(public id: number, public name: string, public created_at: Date) { }
 
-    private getUsersStatement = db.prepare<ProjectUser, [number]>(`
-SELECT u.id, u.email, pu.role
-FROM users u
-JOIN project_users pu ON pu.user_id = u.id
-JOIN projects p ON p.id = pu.project_id
-WHERE p.id = $1
-`)
+    private users!: ProjectUser[]
+    public getUsers(fromCache = true): ProjectUser[] {
+        if (!fromCache || !this.users) {
+            this.users = getProjectUsersStmt().all(this.id)
+        }
 
-    public getUsers(): ProjectUser[] {
-        return this.getUsersStatement.all(this.id)
+        return this.users
     }
 }
 
@@ -23,4 +20,14 @@ export class ProjectUserForm { constructor(public userId: number, public role: P
 
 export class ProjectForm {
     constructor(public name: string, public users: ProjectUserForm[]) { }
+}
+
+function getProjectUsersStmt() {
+    return db.prepare<ProjectUser, [number]>(`
+        SELECT u.id, u.email, pu.role
+        FROM users u
+        JOIN project_users pu ON pu.user_id = u.id
+        JOIN projects p ON p.id = pu.project_id
+        WHERE p.id = $1
+    `)
 }
