ee85990cac553c226f17fb5813ba98f62d74ba57
BrianDouglasIE <briandgls92@gmail.com>
2025-09-05 12:25:52 +0100

    feat: add hono for web requests

--- index.ts
+++ index.ts
@@ -2,9 +2,18 @@ import { Project } from "./models/project";
 import { db } from "./db/db";
 import type { projects_table } from "./sqlite-table-defs";
 
-const rawProjects = db.query<projects_table, []>('SELECT * FROM projects LIMIT 10;').all()
-for (const rawProject of rawProjects) {
-    const project = new Project(rawProject.id, rawProject.name, new Date(rawProject.created_at))
-    console.log(project.name)
-    console.log(project.getUsers())
-}
+import { Hono } from 'hono'
+
+const projects = new Hono().basePath('/projects')
+projects.get('/', (c) => {
+    const rawProjects = db.query<projects_table, []>('SELECT * FROM projects LIMIT 10;').all()
+    const projects = rawProjects.map(it => new Project(it.id, it.name, new Date(it.created_at)))
+    return c.json(projects)
+})
+
+const app = new Hono()
+app.route('/', projects)
+app.get('/', (c) => c.text('Hello Bun!'))
+
+
+export default app
